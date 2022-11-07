c2883bf089bd21321c2fadc3913a7ce97ad65940
wkasem2 <wkasem22@gmail.com>
2022-11-07 19:58:51 +0200

    make it better

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   publicRuntimeConfig: {
     client_id: process.env.CLIENT_ID,
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'eco-lawn',
+    title: 'Eco Lawn',
     htmlAttrs: {
       lang: 'en'
     },
