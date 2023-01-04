352f1bab9f17e634a4001ae880b04f709555e2ac
wkasem2 <wkasem22@gmail.com>
2023-01-04 20:05:08 +0200

    wip

---
+++ .nuxt/components/readme.md
@@ -0,0 +1,16 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<JobCustomerInfo>` | `<job-customer-info>` (components/job/CustomerInfo.vue)
+- `<JobNotes>` | `<job-notes>` (components/job/Notes.vue)
+- `<JobQuoteItems>` | `<job-quote-items>` (components/job/QuoteItems.vue)
+- `<JobSitePlan>` | `<job-site-plan>` (components/job/SitePlan.vue)
+- `<QuoteCustomerInfo>` | `<quote-customer-info>` (components/quote/CustomerInfo.vue)
+- `<QuoteCustomerNotes>` | `<quote-customer-notes>` (components/quote/CustomerNotes.vue)
+- `<QuotePricing>` | `<quote-pricing>` (components/quote/Pricing.vue)
+- `<QuoteTerms>` | `<quote-terms>` (components/quote/Terms.vue)
---
+++ .nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _15ab965f = () => interopDefault(import('..\\pages\\Job.vue' /* webpackChunkName: "pages/Job" */))
+const _720be3f7 = () => interopDefault(import('..\\pages\\Plan.vue' /* webpackChunkName: "pages/Plan" */))
+const _a5aa12d8 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/Job",
+    component: _15ab965f,
+    name: "Job"
+  }, {
+    path: "/Plan",
+    component: _720be3f7,
+    name: "Plan"
+  }, {
+    path: "/",
+    component: _a5aa12d8,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
