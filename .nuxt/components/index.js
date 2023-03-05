79dd7a494bcf6c787cec4d356e2d9792e1864a49
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-03-05 15:17:32 +0200

    deploy

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,11 +1,11 @@
-export const JobCustomerInfo = () => import('../../components/job/CustomerInfo.vue' /* webpackChunkName: "components/job-customer-info" */).then(c => wrapFunctional(c.default || c))
-export const JobNotes = () => import('../../components/job/Notes.vue' /* webpackChunkName: "components/job-notes" */).then(c => wrapFunctional(c.default || c))
-export const JobQuoteItems = () => import('../../components/job/QuoteItems.vue' /* webpackChunkName: "components/job-quote-items" */).then(c => wrapFunctional(c.default || c))
-export const JobSitePlan = () => import('../../components/job/SitePlan.vue' /* webpackChunkName: "components/job-site-plan" */).then(c => wrapFunctional(c.default || c))
-export const QuoteCustomerInfo = () => import('../../components/quote/CustomerInfo.vue' /* webpackChunkName: "components/quote-customer-info" */).then(c => wrapFunctional(c.default || c))
-export const QuoteCustomerNotes = () => import('../../components/quote/CustomerNotes.vue' /* webpackChunkName: "components/quote-customer-notes" */).then(c => wrapFunctional(c.default || c))
-export const QuotePricing = () => import('../../components/quote/Pricing.vue' /* webpackChunkName: "components/quote-pricing" */).then(c => wrapFunctional(c.default || c))
-export const QuoteTerms = () => import('../../components/quote/Terms.vue' /* webpackChunkName: "components/quote-terms" */).then(c => wrapFunctional(c.default || c))
+export const JobCustomerInfo = () => import('../..\\components\\job\\CustomerInfo.vue' /* webpackChunkName: "components/job-customer-info" */).then(c => wrapFunctional(c.default || c))
+export const JobNotes = () => import('../..\\components\\job\\Notes.vue' /* webpackChunkName: "components/job-notes" */).then(c => wrapFunctional(c.default || c))
+export const JobQuoteItems = () => import('../..\\components\\job\\QuoteItems.vue' /* webpackChunkName: "components/job-quote-items" */).then(c => wrapFunctional(c.default || c))
+export const JobSitePlan = () => import('../..\\components\\job\\SitePlan.vue' /* webpackChunkName: "components/job-site-plan" */).then(c => wrapFunctional(c.default || c))
+export const QuoteCustomerInfo = () => import('../..\\components\\quote\\CustomerInfo.vue' /* webpackChunkName: "components/quote-customer-info" */).then(c => wrapFunctional(c.default || c))
+export const QuoteCustomerNotes = () => import('../..\\components\\quote\\CustomerNotes.vue' /* webpackChunkName: "components/quote-customer-notes" */).then(c => wrapFunctional(c.default || c))
+export const QuotePricing = () => import('../..\\components\\quote\\Pricing.vue' /* webpackChunkName: "components/quote-pricing" */).then(c => wrapFunctional(c.default || c))
+export const QuoteTerms = () => import('../..\\components\\quote\\Terms.vue' /* webpackChunkName: "components/quote-terms" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
