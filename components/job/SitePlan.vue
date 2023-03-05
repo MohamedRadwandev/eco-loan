3bb1b2d380f7ab750fcbeb8657188f523c156bbb
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-03-05 15:26:04 +0200

    deploy

--- components/job/SitePlan.vue
+++ components/job/SitePlan.vue
@@ -2,7 +2,7 @@
      <b-container>
           <p class="h3 font-weight-bold">Plan</p>
           <div class="text-center" >
-               <div class="" v-viewer="{inline: true}">
+               <div class="" v-viewer>
                     <img src="~/static/imgs/plan.png"/>
                <!-- <b-img alt="Plan" fluid></b-img> -->
                <!-- <vue-image-zoomer regular="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" /> -->
