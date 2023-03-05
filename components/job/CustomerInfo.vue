ffb7c47021fe765ddbb7d4817ec82b192fb613a9
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-03-05 10:50:30 +0200

    add customer phone

--- components/job/CustomerInfo.vue
+++ components/job/CustomerInfo.vue
@@ -4,19 +4,24 @@
       <b-row>
         <b-col cols="12" md="6">
           <p class="h1 font-weight-bold">Job Reference Number</p>
-          <p class="h1 font-weight-bold">6085</p>
+          <p class="h1 font-weight-bold">{{ quote.id }}</p>
         </b-col>
 
         <b-col cols="12" md="6" class="text-left mt-5 mt-md-0">
           <p class="h3 font-weight-bold mb-4">Customer info</p>
           <div class="mb-4">
             <p class="h5 font-weight-bold">Name</p>
-            <p class="h5">Victor Du Toit</p>
+            <p class="h5">{{ quote.customer_name }}</p>
           </div>
 
           <div>
+            <p class="h5 font-weight-bold">Phone</p>
+            <p class="h5">{{ quote.customer_phone }}</p>
+          </div>
+          
+          <div>
             <p class="h5 font-weight-bold">address</p>
-            <p class="h5">9 Dida Park Drive, Kume 0891, New Zealand</p>
+            <p class="h5">{{ quote.site_address | emptyDash }}</p>
           </div>
         </b-col>
       </b-row>
@@ -26,5 +31,19 @@
 
 <script>
 export default {
-}
+  inject: ["quote"],
+  computed: {
+    name() {
+      const name = this.quote.customer_name.split(" ");
+
+      return {
+        first: name[0],
+        last: name[1],
+      };
+    },
+    created_date() {
+      return new Date(this.quote.quote_create_date).toLocaleDateString();
+    },
+  },
+};
 </script>
