b97905357941a92bb63fe4893be3565cc9c3a9bc
Ahmed Elwerdany <infoalwerdany@gmail.com>
2023-03-05 11:09:55 +0200

    add expand all

--- components/job/QuoteItems.vue
+++ components/job/QuoteItems.vue
@@ -11,31 +11,46 @@
       @row-clicked="toggleCollapse"
       details-td-class="d-md-none pt-0"
     >
+      <template #head(name)>
+        Name
+        <b-btn
+          class="text-uppercase font-weight-bold ml-4 d-none d-md-inline-block"
+          variant="outline-primary"
+          size="sm"
+          @click="toggleCollapseAll"
+        >
+          {{ allColsExpanded || expandAll ? "Collapse All" : "Expand All" }}
+          <b-icon
+            class="font-weight-bold ml-2"
+            :icon="allColsExpanded || expandAll ? 'chevron-up' : 'chevron-down'"
+          ></b-icon>
+        </b-btn>
+      </template>
       <template #cell(name)="row">
-          <div class="d-flex align-items-start cursor-pointer">
-            <p class="h5 mb-0 font-weight-bold d-inline-block">
-              {{ row.item.name }}
-            </p>
-            <b-icon
-              class="text-dark ml-4"
-              :icon="row.detailsShowing ? 'chevron-up' : 'chevron-down'"
-            >
-            </b-icon>
-          </div>
-
-          <p class="h5 mt-2 d-none d-md-block" v-if="row.item._showDetails">
-            {{ row.item.description }}
+        <div class="d-flex align-items-start cursor-pointer">
+          <p class="h5 mb-0 font-weight-bold d-inline-block">
+            {{ row.item.name }}
           </p>
+          <b-icon
+            class="text-dark ml-4"
+            :icon="row.detailsShowing ? 'chevron-up' : 'chevron-down'"
+          >
+          </b-icon>
+        </div>
+
+        <p class="h5 mt-2 d-none d-md-block" v-if="row.item._showDetails">
+          {{ row.item.description }}
+        </p>
       </template>
 
       <template #cell(quantity)="row">
         <p class="h5">{{ row.item.quantity }} M</p>
       </template>
-     <template #row-details="row">
-          <p class="h5">
-               {{ row.item.description }}
-          </p>
-     </template>
+      <template #row-details="row">
+        <p class="h5">
+          {{ row.item.description }}
+        </p>
+      </template>
     </b-table>
   </b-container>
 </template>
@@ -49,10 +64,10 @@ export default {
   },
   data() {
     return {
-         fields: [
-              { key: "name", tdClass: "w-75 pl-0" , thClass:"pl-0" },
-              { key: "quantity", tdClass: "text-right", thClass:"text-right" },
-         ],
+      fields: [
+        { key: "name", tdClass: "w-75 pl-0", thClass: "pl-0" },
+        { key: "quantity", tdClass: "text-right", thClass: "text-right" },
+      ],
       items: [
         {
           name: "H4 timber containment",
@@ -78,10 +93,18 @@ export default {
       ],
     };
   },
-
+  created () {
+    this.toggleCollapseAll()
+  },
   methods: {
     toggleCollapse(item) {
       item._showDetails = !item._showDetails;
+    },
+    toggleCollapseAll() {
+      this.expandAll = !this.expandAll;
+      this.items.forEach((item) => {
+        item._showDetails = this.expandAll;
+      });
     },
   },
 };
